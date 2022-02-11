b6a467fcca346f35c18924800f3e3b7bb1d25861
Ilton Silveira <iltonsilveira91@gmail.com>
2022-02-11 02:31:10 -0300

    readded initial token source code

---
+++ README.md
@@ -0,0 +1,7 @@
+# DeFi
+
+1. Mint stable coin giving ether as collateral
+1. Burn stable coin receiving ether back
+1. Calculate it collateral based on real ether price based on uniswap
+1. Calculate the risk to decide the ratio autonomously
+
---
+++ mint_dollar.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
+
+contract MintDollar is ERC20 {
+
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        // Mint 100 tokens to msg.sender = 100 * 10**uint(decimals())
+        // Mint 100.000.000 tokens to msg.sender = 100000000 * 10**uint(decimals())
+        // Similar to how
+        // 1 dollar = 100 cents
+        // 1 token = 1 * (10 ** decimals)
+        // 100 * 10**uint(decimals()) == 100 units and 100000000000000000000 min units
+        // 100000000 * 10**uint(decimals()) == 100.000.000 units and 100000000000000000000 min units
+        _mint(msg.sender, 1000 * 10**uint(decimals()));
+    }
+
+    
+
+}
